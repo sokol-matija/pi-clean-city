5902a781c3f20aa11f97d646e70601e8a54431e7
dantic1 <dantic1@algebra.hr>
2025-12-10 22:24:44 +0100

    Add GeoLocation entity

---
+++ src/CleanCity.Domain/Entities/GeoLocation.cs
@@ -0,0 +1,15 @@
+namespace CleanCity.Domain.Entities;
+
+public class GeoLocation
+{
+    public int Id { get; set; }
+    public string Address { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string PostalCode { get; set; } = string.Empty;
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
+
+    //nav prop
+    public int TicketId { get; set; }
+    public Ticket Ticket { get; set; } = null!;
+}
