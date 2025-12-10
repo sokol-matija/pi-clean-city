3a83940dfec2fe137c8f344a4e9adf3a9c5e9a86
dantic1 <dantic1@algebra.hr>
2025-12-10 22:24:14 +0100

    Add User entity

---
+++ src/CleanCity.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+using CleanCity.Domain.Enums;
+
+namespace CleanCity.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public UserRole Role { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsActive { get; set; } = true;
+
+    //nav prop
+    public ICollection<Ticket> CreatedTickets { get; set; } = new List<Ticket>();
+}
