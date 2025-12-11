b493f21e80212c74a1b32e14bdc1658bbdf7d64e
Domagoj <dantic1@algebra.hr>
2025-12-11 21:04:57 +0100

    Add AssignTicketDto

---
+++ src/CleanCity.Application/DTOs/Tickets/AssignTicketDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCity.Application.DTOs.Tickets
+{
+    public class AssignTicketDto
+    {
+        public int CityServiceId { get; set; }
+    }
+}
