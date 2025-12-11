f6a01138eaa04a6adda2749eed635114dadd0f23
Domagoj <dantic1@algebra.hr>
2025-12-11 23:41:28 +0100

    Add Country property

--- src/CleanCity.Domain/Entities/GeoLocation.cs
+++ src/CleanCity.Domain/Entities/GeoLocation.cs
@@ -3,6 +3,7 @@ namespace CleanCity.Domain.Entities;
 public class GeoLocation
 {
     public int Id { get; set; }
+    public string Country { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
     public string City { get; set; } = string.Empty;
     public string PostalCode { get; set; } = string.Empty;
