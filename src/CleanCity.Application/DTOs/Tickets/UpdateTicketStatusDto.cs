c6b5ec4c26cb8c5d01e326e9c846289f7a15d8c8
Domagoj <dantic1@algebra.hr>
2025-12-11 21:03:35 +0100

    Add UpdateTicketStatusDto

---
+++ src/CleanCity.Application/DTOs/Tickets/UpdateTicketStatusDto.cs
@@ -0,0 +1,15 @@
+﻿using CleanCity.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCity.Application.DTOs.Tickets
+{
+    public class UpdateTicketStatusDto
+    {
+        public TicketStatus Status { get; set; }
+        public string? Notes { get; set; }
+    }
+}
