db5260f1c240cc731b329e2c1d633c1ede4abb3c
Domagoj <dantic1@algebra.hr>
2025-12-11 22:01:51 +0100

    Add AuthController

---
+++ src/CleanCity.API/Controllers/AuthController.cs
@@ -0,0 +1,74 @@
+﻿using CleanCity.Application.DTOs.Auth;
+using CleanCity.Application.Services;
+using CleanCity.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanCity.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly CleanCityDbContext _context;
+        private readonly IJwtService _jwtService;
+
+        public AuthController(CleanCityDbContext context, IJwtService jwtService)
+        {
+            _context = context;
+            _jwtService = jwtService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginRequestDto>> Login([FromBody] LoginRequestDto request)
+        {
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == request.Email);
+
+            if (user == null)
+                return Unauthorized(new {message = "Invalid email or password." });
+
+            //TODO: Replace with Bcrypt
+            if (request.Password != "admin123")
+                return Unauthorized(new { message = "Invalid email or password." });
+
+            var token = _jwtService.GenerateToken(user);
+
+            var response = new LoginResponseDto
+            {
+                Token = token,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Role = user.Role.ToString(),
+                ExpiresAt = DateTime.UtcNow.AddMinutes(60)
+            };
+
+            return Ok(response);
+        }
+
+        [HttpGet("me")]
+        [Microsoft.AspNetCore.Authorization.Authorize]
+        public async Task<ActionResult<LoginResponseDto>> GetCurrentUser()
+        {
+           var userId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+                return Unauthorized();
+
+            var user = await _context.Users.FindAsync(int.Parse(userId));
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(new
+            {
+                user.Id,
+                user.FirstName,
+                user.LastName,
+                user.Email,
+                Role = user.Role.ToString(),
+            });
+        }
+    }
+}
