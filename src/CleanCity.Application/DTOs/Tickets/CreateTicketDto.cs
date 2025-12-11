f7e226a3dcff5e4e66d4eed05d9f74229b28a365
Domagoj <dantic1@algebra.hr>
2025-12-11 23:11:31 +0100

    Add CreateTicketDto

---
+++ src/CleanCity.Application/DTOs/Tickets/CreateTicketDto.cs
@@ -0,0 +1,19 @@
+﻿using CleanCity.Domain.Enums;
+
+namespace CleanCity.Application.DTOs.Tickets;
+
+public class CreateTicketDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int CategoryId { get; set; }
+    public Priority Priority { get; set; } = Priority.Medium; 
+
+    public string Address { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+
+    // img optional
+    public string? ImageUrl { get; set; }
+}
