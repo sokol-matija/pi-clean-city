1a7de8fe61ec050b1341a263551a99542d3ed5f7
Domagoj <dantic1@algebra.hr>
2025-12-12 13:18:30 +0100

    Add CategoryController

---
+++ src/CleanCity.API/Controllers/CategoriesController.cs
@@ -0,0 +1,54 @@
+﻿using CleanCity.Application.DTOs.Categories;
+using CleanCity.Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanCity.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly CleanCityDbContext _context;
+
+        public CategoriesController(CleanCityDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> GetCategories()
+        {
+            var categories = await _context.Categories
+                .Select(c => new CategoryDto
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Description = c.Description
+                })
+                .ToListAsync();
+
+            return Ok(categories);
+        }
+
+        [HttpGet("id")]
+        public async Task<ActionResult<CategoryDto>> GetCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+                return NotFound();
+
+            var categotyDto = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Description = category.Description
+            };
+
+            return (categotyDto);
+        }
+    }
+}
