e806fab47aad0cc52dee05db3bcea584691f6ede
Domagoj Antić <161882035+dantic1@users.noreply.github.com>
2025-12-19 13:28:25 +0000

    feat: add TicketServiceContext for dependency injection of ITicketService

---
+++ src/features/admin/context/TicketServiceContext.tsx
@@ -0,0 +1,33 @@
+/* 
+    Ticket Service Context
+    Provides dependecy injection for ITicketService
+*/
+
+import { createContext, useContext, type ReactNode } from "react"
+import type { ITicketService } from "../services"
+import { SupabaseTicketService } from "../services"
+
+const TicketServiceContext = createContext<ITicketService | undefined>(undefined);
+
+interface TicketServiceProviderProps {
+    children: ReactNode;
+    service?: ITicketService;
+}
+
+export function TicketServiceProvider({ children, service = new SupabaseTicketService()}: TicketServiceProviderProps) {
+    return (
+        <TicketServiceContext.Provider value={service}>
+            {children}
+        </TicketServiceContext.Provider>
+    )
+}
+
+export function useTicketService(): ITicketService {
+    const context = useContext(TicketServiceContext);
+    if (!context) {
+        throw new Error("useTicketService must be used within a TicketServiceProvider");
+    }
+    return context;
+}   
+
+
