9881cae1dac48a77262e0f206d608e75cfeb11c0
Fran <fran.vicic@gmail.com>
2026-01-17 11:53:30 +0100

    feat: security vulnerabilities resolved

--- MobileApp/CleanCity.Core/Services/InsecureHttpClient.cs
+++ MobileApp/CleanCity.Core/Services/InsecureHttpClient.cs
@@ -3,10 +3,23 @@ namespace CleanCity.Core.Services;
 
 public static class InsecureHttpClient
 {
+    //// =====================================================================================
+    //// SIGURAN KOD
+    //// =====================================================================================
+    //public static HttpClient CreateSecureClient()
+    //{
+    //    return new HttpClient();
+    //}
+
+    // =====================================================================================
+    // NESIGURAN KOD - SONARQUBE VULNERABILITY S4830
+    // =====================================================================================
     public static HttpClient CreateInsecureClient()
     {
         var handler = new HttpClientHandler
         {
+            // The following line disables server certificate validation,
+            // making the connection vulnerable to man-in-the-middle attacks.
             ServerCertificateCustomValidationCallback =
                 HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
         };
